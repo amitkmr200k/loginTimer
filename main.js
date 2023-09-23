7de464486c7490ffeee175a868ae3dffcdff581f
Amit Kumar <amit.kumar29@livpure.com>
2023-09-23 18:30:17 +0530

    added now timer

--- main.js
+++ main.js
@@ -10,36 +10,54 @@ const logoutTimeFormattedEle = document.getElementById("logoutTimeFormatted");
 const pendingEle = document.getElementById("pending");
 const doneEle = document.getElementById("passedTimeMsg");
 const parentDisplayEle = document.getElementById("parentDisplay");
+const nowEle = document.getElementById("now");
 
-loginTimeEle.addEventListener("click", checkTime);
-loginTimeEle.addEventListener("change", checkTime);
-shiftHourEle.addEventListener("click", checkTime);
-shiftHourEle.addEventListener("change", checkTime);
-shiftMinutesEle.addEventListener("click", checkTime);
-shiftMinutesEle.addEventListener("change", checkTime);
+loginTimeEle.addEventListener("click", calculateTime);
+loginTimeEle.addEventListener("change", calculateTime);
+shiftHourEle.addEventListener("click", calculateTime);
+shiftHourEle.addEventListener("change", calculateTime);
+shiftMinutesEle.addEventListener("click", calculateTime);
+shiftMinutesEle.addEventListener("change", calculateTime);
+nowEle.addEventListener("click", clickedNowBtton);
+nowEle.addEventListener("change", clickedNowBtton);
 
 resetAll();
 
-function checkTime() {
+function clickedNowBtton() {
+    loginTimeStr = moment().format("H:mm");
+
+    loginTimeEle.value = loginTimeStr;
+
+    loginTimeEle.click();
+}
+
+async function calculateTime() {
     localStorage.setItem("clickedByUser", true);
 
-    clearAllIntervals();
+    await clearAllIntervals();
 
-    const loginTime = loginTimeEle.value;
+    let loginTimeStr = loginTimeEle.value;
 
-    if (loginTime) {
+    if (loginTimeStr) {
         const shiftHour = shiftHourEle.value || 8;
         const shiftMinute = shiftMinutesEle.value || 30;
 
         localStorage.setItem("shiftHour", shiftHour);
         localStorage.setItem("shiftMinute", shiftMinute);
-        localStorage.setItem("loginTime", loginTime);
+        localStorage.setItem("loginTimeStr", loginTimeStr);
 
-        const login = moment(loginTime, "H:mm");
-        const end = moment(loginTime, "H:mm").add(shiftHour, "h").add(shiftMinute, "m");
+        const defaultLoginTimeStr = moment("09:30", "H:mm");
+        let loginTime = moment(loginTimeStr, "H:mm");
+
+        if (loginTime.diff(moment(defaultLoginTimeStr), "seconds") < 0) {
+            loginTime = defaultLoginTimeStr;
+        }
+
+        const end = loginTime.clone().add(shiftHour, "h").add(shiftMinute, "m");
+        // console.log("🚀 ~ file: main.js:56 ~ calculateTime ~ loginTime:", loginTime)
 
         parentDisplayEle.style.display = "block";
-        loginTimeFormattedEle.value = login.format("YYYY-MM-DD hh:mm A");
+        loginTimeFormattedEle.value = loginTime.format("YYYY-MM-DD hh:mm A");
         logoutTimeFormattedEle.value = end.format("YYYY-MM-DD hh:mm A");
 
         let differenceSeconds = end.diff(moment(), "seconds");
@@ -54,7 +72,9 @@ function checkTime() {
         } else {
             const { hours, minutes, seconds } = secondsToHms(differenceSeconds);
 
+            pendingEle.style.display = "block";
             pendingEle.classList.add("show");
+
             pendingEle.innerText = "Time Remaning " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
 
             startNotPassedInterval(differenceSeconds);
@@ -89,7 +109,7 @@ function startPassedInterval(timer) {
     if (timer === 1) {
         doneEle.style.display = "block";
         doneEle.classList.add("show");
-    
+
         doneEle.innerText = "Time Passed " + 0 + " hours " + 0 + " minutes " + 1 + " seconds ago.";
     }
 
@@ -141,16 +161,34 @@ function clearAllIntervals() {
 }
 
 function resetAll() {
-    loginTimeEle.value = localStorage.getItem("loginTime") || "09:30";
+    // if (!localStorage.getItem("calledByNowButton")) {
+        loginTimeEle.value = localStorage.getItem("loginTimeStr") || "09:30";
+    // } else {
+    //     loginTimeEle.value = "09:30";
+    // }
+
+    // loginTimeEle.value = localStorage.getItem("loginTimeStr") || !localStorage.getItem("calledByNowButton") ? "09:30": localStorage.getItem("loginTimeStr");
     shiftHourEle.value = localStorage.getItem("shiftHour") || 8;
     shiftMinutesEle.value = localStorage.getItem("shiftMinute") || 30;
 
-    if (localStorage.getItem("clickedByUser")) {
+    let setDate = localStorage.getItem("currentDate");
+
+    if (!setDate) {
+        localStorage.setItem("currentDate", moment().format("YYYY-MM-DD"));
+    }
+
+    setDate = moment(localStorage.getItem("currentDate"));
+
+    let diffDate = setDate.diff(moment(), "days");
+
+    if (localStorage.getItem("clickedByUser") && diffDate >= 0) {
         loginTimeEle.click();
     } else {
         parentDisplayEle.style.display = "none";
         pendingEle.style.display = "none";
         doneEle.style.display = "none";
+
+        localStorage.clear();
     }
 
     clearAllIntervals();
